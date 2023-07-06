a4b4edc20afe500f845bcb80d4b14494846aa9e5
lattalab <91266449+lattalab@users.noreply.github.com>
2023-07-06 13:05:28 +0800

    Update Huffman.cpp

--- hw8(redo)/Huffman.cpp
+++ hw8(redo)/Huffman.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[])
 {
     if (strcmp(argv[1], "-c") == 0)
-        compress(argv[3]);
+        compress(argv[3],argv[5]);
     if (strcmp(argv[1], "-u") == 0)
         decompress(argv[3],argv[5]);
     
